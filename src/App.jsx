505f1f6c0899b9cde7ad777999e31dff239df778
mohamed ayad <126675322+mohamed-hussein-ayad@users.noreply.github.com>
2024-09-14 13:53:00 +0300

    create some components and build the sidebar and the undefind page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
 import ProjectsSidebar from "./components/ProjectsSidebar";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import { useState } from "react";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
-    <>
-      <ProjectsSidebar />
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
+    </main>
   );
 }
 
--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,9 +1,13 @@
-export default function ProjectsSidebar() {
+import Button from "./Button";
+
+export default function ProjectsSidebar({ onStartAddProject }) {
   return (
-    <aside>
-      <h2>Your Projects</h2>
+    <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+      <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
+        Your Projects
+      </h2>
       <div>
-        <button>+ Add Project</button>
+        <Button onClick={onStartAddProject}>+ Add Project</Button>
       </div>
       <ul></ul>
     </aside>
